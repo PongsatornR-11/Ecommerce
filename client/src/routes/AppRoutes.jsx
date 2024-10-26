38b4cc18bec259a0dc8a6205615d5f4636843c69
WomenOnTOP <69459885+WomenOnTOP@users.noreply.github.com>
2024-10-26 23:08:35 +0700

    commit

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,37 +1,72 @@
-import React from 'react'
+//rafce
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from '../pages/Home'
-import Shop from '../pages/Shop'
-import Cart from '../pages/Cart'
-import History from '../pages/History'
-import Checkout from '../pages/Checkout'
-import Login from '../pages/auth/login'
-import Register from '../pages/auth/Register'
-import Layout from '../layouts/Layout'
+// import pages form folder 'pages'
+import Home from "../pages/Home";
+import Shop from "../pages/Shop";
+import Cart from "../pages/Cart";
+import History from "../pages/History";
+import Checkout from "../pages/Checkout";
+import Login from "../pages/auth/login";
+import Register from "../pages/auth/Register";
+import Dashboard from "../pages/admin/Dashboard";
+import Category from "../pages/admin/Category";
+import Product from "../pages/admin/Product";
+import Manage from '../pages/admin/Manage'
+import HomeUser from "../pages/user/HomeUser";
+
+//import layouts from folder 'layouts'
+import Layout from "../layouts/Layout";
+import LayoutAdmin from "../layouts/LayoutAdmin";
+import LayoutUser from "../layouts/LayoutUser";
+
 
 const router = createBrowserRouter([
-    { 
-        path: '/', 
-        element:<Layout/>,
-        children:[
-            { index: true, element:<Home /> },
-            { path: 'shop', element:<Shop /> },
-            { path: 'cart', element:<Cart />},
-            { path: 'history', element:<History />},
-            { path: 'checkout', element:<Checkout />},
-            { path: 'login', element:<Login />},
-            { path: 'register', element:<Register />},
-        ]
-    },
-])
+  // public path
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "shop", element: <Shop /> },
+      { path: "cart", element: <Cart /> },
+      { path: "history", element: <History /> },
+      { path: "checkout", element: <Checkout /> },
+      { path: "login", element: <Login /> },
+      { path: "register", element: <Register /> },
+    ],
+  },
+
+  // admin path
+  {
+    path: "/admin",
+    element: <LayoutAdmin />,
+    children: [
+      { index: true, element: <Dashboard /> },
+      { path: "category", element: <Category /> },
+      { path: "product", element: <Product /> },
+      { path: "manage", element: <Manage /> },
+    ],
+  },
+
+  // user path
+  {
+    path: '/user',
+    element: <LayoutUser />,
+    children: [
+      { index: true, element:<HomeUser />},
+    ]
+  }
+  
+]);
 
 const AppRoutes = () => {
   return (
     <>
-        <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </>
-  )
-}
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
