3dd894b63b8a511d0bae6047668b963e7c107249
WomenOnTOP <69459885+WomenOnTOP@users.noreply.github.com>
2024-10-31 21:16:59 +0700

    commit

--- client/src/components/admin/FromProduct.jsx
+++ client/src/components/admin/FromProduct.jsx
@@ -6,7 +6,12 @@ const FromProduct = () => {
 
     const token = useEcomStore((state) => state.token)
     const getCategory = useEcomStore((state)=>state.getCategory)
+    const categories = useEcomStore((state)=>state.categories)
 
+    useEffect(()=>{
+        getCategory(token)
+    },[])
+    console.log(categories)
     return (
         <div>FromProduct</div>
     )
