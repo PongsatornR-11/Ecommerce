f6ded614894f1572584e12bf4f2e7b23ec462f2b
WomenOnTOP <69459885+WomenOnTOP@users.noreply.github.com>
2024-11-24 23:14:42 +0700

    Add Search by Category function

--- client/src/components/card/SearchCard.jsx
+++ client/src/components/card/SearchCard.jsx
@@ -9,36 +9,72 @@ const SearchCard = () => {
 
     const getCategory = useEcomStore((state) => state.getCategory)
     const categories = useEcomStore((state) => state.categories)
-    console.log(categories)
-    const [text, setText] = useState('')
 
-    // step 1 Search by Text
+    const [text, setText] = useState('')
+    const [categoryChecked, setCategoryChecked] = useState([])
 
     useEffect(() => {
+        getCategory()
+    }, [])
+
+    // step 1 Search by Text
+    useEffect(() => {
         const delay = setTimeout(() => {
-            actionSearchFilter({ query: text })
-            if (!text) {
+            if (text) {
+                actionSearchFilter({ query: text })
+            } else {
                 getProduct()
             }
         }, 300);
-
         return () => clearTimeout(delay)
     }, [text])
 
     // step 2 Search by Cetegory
-
+    const handleCheckbox = (e) => {
+        const inCheck = e.target.value
+        const inState = [...categoryChecked]
+        const findCheck = inState.indexOf(inCheck)
+        if (findCheck === -1) {
+            inState.push(inCheck)
+        } else {
+            inState.splice(findCheck, 1)
+        }
+        setCategoryChecked(inState)
+        if (inState.length > 0) {
+            actionSearchFilter({ category: inState })
+        } else {
+            getProduct()
+        }
+    }
     // step 3 Search by price
 
     return (
         <div>
             <h1 className='text-xl font-bold mb-4'>Find Product</h1>
-
+            {/* Search by Text */}
             <input
                 onChange={(e) => setText(e.target.value)}
                 type='text'
                 placeholder='Find Product...'
                 className='border rounded-md w-full mb-4'
             />
+            <hr />
+            {/* Search by Category */}
+            <div>
+                <h1>Categories</h1>
+                <div>
+                    {categories.map((item, index) =>
+                        <div className='flex items-center gap-2' key={index}>
+                            <input
+                                type='checkbox'
+                                value={item.id}
+                                onChange={handleCheckbox}
+                            />
+                            <label>{item.name}</label>
+                        </div>
+                    )}
+                </div>
+            </div>
         </div>
     )
 }
