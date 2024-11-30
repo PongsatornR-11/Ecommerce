5ff98c6a0d74253dbffc14f3444c4293fe8b13ea
WomenOnTOP <69459885+WomenOnTOP@users.noreply.github.com>
2024-11-30 17:57:07 +0700

    add start up project command for back up

---
+++ Start up project.bat
@@ -0,0 +1,19 @@
+@echo off
+start wt new-tab -p "Command Prompt" -d "C:\Learn coding\web coding\Projects\Ecommerce\client" cmd.exe /k "npm run dev"^
+    ; new-tab -p "Command Prompt" -d "C:\Learn coding\web coding\Projects\Ecommerce\server" cmd.exe /k "npm start"
+
+start wt new-tab -p "Command Prompt" -d "C:\Learn coding\web coding\Projects\Ecommerce" cmd.exe /c "cursor ."
+
+:: Wait a bit to ensure Windows Terminal finishes opening all tabs before launching the next program
+timeout /t 3 >nul
+:: Open Postman
+start "" "C:\Users\PC\AppData\Local\Postman\Postman.exe"
+
+
+
+:: Open Web project
+start "C:\Program Files\Google\Chrome\Application\chrome.exe" "http://localhost:5173"
+:: Open Lofi girl
+start "C:\Program Files\Google\Chrome\Application\chrome.exe" "https://www.youtube.com/watch?v=jfKfPfyJRdk"
+:: Open roithai
+start "C:\Program Files\Google\Chrome\Application\chrome.exe" "https://www.youtube.com/watch?v=zoEgIhrY4cU&list=PL3CRqF9WxjEZHFnZSSdC4wNDgvkKhpMtS&index=21"
