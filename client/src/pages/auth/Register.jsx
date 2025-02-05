8184f7ba82e9f07a4265e27ca87457721f24bc7d
WomenOnTOP <69459885+WomenOnTOP@users.noreply.github.com>
2025-02-05 21:49:48 +0700

    Update dependencies and registration page
    - Updated `client/package-lock.json` and `client/package.json` to the latest versions.
    - Modified `client/src/pages/auth/Register.jsx` to improve the registration process.
    
    Date: [Insert Current Date]

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -5,65 +5,78 @@ import axios from "axios";
 
 import { toast } from "react-toastify";
 
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+
+import zxcvbn from 'zxcvbn' // use .score to check score of password
+
+const registerSchema = z
+    .object({
+        email: z.string().email({ message: 'Invalid email!' }),
+        password: z.string().min(8, { message: 'Password must have at least 8 letters' }),
+        confirmPassword: z.string()
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+        message: 'Password is not match',
+        path: ["confirmPassword"],
+    })
+
 const Register = () => {
-  const [form, setForm] = useState({
-    email: "",
-    password: "",
-    confirmPassword: "",
-  });
 
-  const handleOnchange = (event) => {
-    setForm({
-      ...form,
-      [event.target.name]: event.target.value,
-    });
-  };
+    const { register, handleSubmit, watch, formState: { errors }, } = useForm({
+        resolver: zodResolver(registerSchema)
+    })
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    if (form.password !== form.confirmPassword) {
-      return alert("Password is not match!!");
+    // new
+    const onSubmit = async (data) => {
+        console.log(data)
+        const passwordScore = zxcvbn(data.password).score // check password score
+        console.log(passwordScore)
+        if (passwordScore < 3) {
+            toast.warning('Password to weak')
+            return
+        }
+        try {
+            const res = await axios.post("http://localhost:5000/api/register", data);
+            toast.success(res.data);
+        } catch (err) {
+            const errMsg = err.response?.data?.message;
+            toast.error(errMsg);
+        }
     }
-    // send to back end.
-    try {
-      const res = await axios.post("http://localhost:5000/api/register", form);
-      toast.success(res.data);
-    } catch (err) {
-      const errMsg = err.response?.data?.message;
-      toast.error(errMsg);
-    }
-  };
 
-  return (
-    <div>
-      Register
-      <form onSubmit={handleSubmit}>
-        Email
-        <input
-          onChange={handleOnchange}
-          className="border"
-          name="email"
-          type="email"
-          placeholder="Enter Email here"
-        />
-        Password
-        <input
-          onChange={handleOnchange}
-          className="border"
-          name="password"
-          type="text"
-        />
-        Confirm Password
-        <input
-          onChange={handleOnchange}
-          className="border"
-          name="confirmPassword"
-          type="text"
-        />
-        <button className="bg-blue-400 rounded-md">Register</button>
-      </form>
-    </div>
-  );
+    return (
+        <div>
+            Register
+            <form onSubmit={handleSubmit(onSubmit)}>
+                Email
+                <input {...register('email')} className="border" />
+                {errors.email &&
+                    <p className="text-red-500 text-sm">
+                        {errors.email.message}
+                    </p>
+                }
+
+                Password
+                <input {...register('password')} className="border" />
+                {errors.password &&
+                    <p className="text-red-500 text-sm">
+                        {errors.password.message}
+                    </p>
+                }
+                Confirm Password
+                <input {...register('confirmPassword')} className="border" />
+                {errors.confirmPassword &&
+                    <p className="text-red-500 text-sm">
+                        {errors.confirmPassword.message}
+                    </p>
+                }
+                <button className="bg-blue-400 rounded-md">Register</button>
+            </form>
+        </div>
+    );
 };
 
 export default Register;
